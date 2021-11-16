4f28b0c3db8fa5049a8a74048df787d4be856145
tanapatpusiri <tanapatpusiri@mail.kmutt.ac.th>
2021-11-16 22:26:01 +0700

    page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Firstpage from './firstpage';
+import Firstpage, { Page } from './firstpage';
 import reportWebVitals from './reportWebVitals';
 import Pagetwo from './twopage';
 import Navbar from './nav.js';
@@ -13,6 +13,7 @@ import Pagesix from './sixpage';
 import Pageseven from './sevenpage';
 import Pageeight from './eight'
 import Pagenine from './nine'
+import Pageten from './ten';
 import Banner from './banner';
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
@@ -29,6 +30,7 @@ ReactDOM.render(
     <Pageseven/>
     <Pageeight/>
     <Pagenine/>
+    <Pageten/>
 
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/ten.js
@@ -0,0 +1,17 @@
+import styled from  'styled-components' ;
+import './ten.css'
+
+
+
+function Pageten(){
+    return(
+        <div className="contain">
+        
+  
+        </div>
+ 
+    )
+ 
+ 
+ }
+ export default Pageten
