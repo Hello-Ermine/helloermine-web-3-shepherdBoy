46372b14c9ede4fb80f23868e9b6f737d26c2241
Fah <phurichayakhem1@gmail.com>
2021-11-18 00:23:17 +0700

    member

--- src/ten.js
+++ src/ten.js
@@ -1,125 +1,57 @@
-import styled from  'styled-components' ;
+import styled from 'styled-components';
 import './ten.css'
-import fence from './fence.png'
+import fence from './fencegroup.png'
+import aomsin from './aomsin.png'
+import gift from './gift.png'
+import fah from './fah.png'
+import bom from './bom.png'
+import boat from './boat.png'
+import toey from './toey.png'
 import shepmember from './shepmember.png'
 
-const Fence = styled.img`
-position: absolute;
-top: 75%;
-left: -1%;
-width:1022px; 
-height:300px;
 
-`
-const Fence1 = styled.img`
-position: absolute;
-top: 75%;
-left: 39%;
-width:1022px; 
-height:300px;
-
-`
-const Fence2 = styled.img`
-position: absolute;
-top: 75%;
-left: 80%;
-width:1022px; 
-height:300px;
-
-`
-
-
-const Shepmember = styled.img`
-position: absolute;
-width:250px; 
-height:250px;
-left: 5%;
-
-`
-const Shepmember1 = styled.img`
-position: absolute;
-width:250px; 
-height:250px;
-left: 20%;
-
-`
-const Shepmember2 = styled.img`
-position: absolute;
-width:250px; 
-height:250px;
-left: 35%;
-
-`
-const Shepmember3 = styled.img`
-position: absolute;
-width:250px; 
-height:250px;
-left: 50%;
-
-`
-const Shepmember4 = styled.img`
-position: absolute;
-width:250px; 
-height:250px;
-left: 65%;
-
-`
-const Shepmember5 = styled.img`
-position: absolute;
-width:250px; 
-height:250px;
-left: 80%;
-
-`
-
-function Pageten(){
-    return(
-        
-        <div className="contain3" id="member">
-        
-     <h1>Member</h1>
-        
-         <div class="grid-container">
-        
-         <div class="grid-item">asdasd</div>
-         <Fence src={fence}/>
-         <Fence1 src={fence}/>
-         <Fence2 src={fence}/>
-         <Shepmember src={shepmember}/>
-
-         <div class="grid-item"></div>
-         <Shepmember1 src={shepmember}/>
-         <div class="grid-item"></div>  
-         <Shepmember2 src={shepmember}/>
-         <div class="grid-item"></div>
-         <Shepmember3 src={shepmember}/>
-         <div class="grid-item"></div>
-         <Shepmember4 src={shepmember}/>
-         <div class="grid-item"></div>  
-         <Shepmember5 src={shepmember}/>
-    
-       </div>
-       <div class="grid-container2">
-        
-        <div class="grid-item1">Aomsin</div>
-  
-
-        <div class="grid-item1">Gift</div>
-       
-        <div class="grid-item1">Fah</div>  
-      
-        <div class="grid-item1">Bom</div>
-      
-        <div class="grid-item1">Boat</div>
-      
-        <div class="grid-item1">Toey</div>  
-     
+const pageTen2 = () => {
+    return (
+        <div id="member" style={{ width: "100vw", minHeight: "100vh", display: "flex", flexDirection: "column", alignItems: "center", background: "#A5DAE2", justifyContent: "space-between" }} >
+            <div>
+                <h1> Member </h1>
+            </div>
+            <div id="gridthing" style={{ display: "grid", width: "97vw" }}  >
+                <div style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <h3> AOMSIN </h3>
+                    <img src={aomsin} style={{ objectFit: "cover", width: "100%" }} />
+                    <h2 className="subText" > WEB DESIGN </h2>
+                </div>
+                <div style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <h3> GIFT </h3>
+                    <img src={gift} style={{ objectFit: "cover", width: "100%" }} />
+                    <h2 className="subText" > WEB DESIGN </h2>
+                </div>
+                <div style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <h3> FAH </h3>
+                    <img src={fah} style={{ objectFit: "cover", width: "100%" }} />
+                    <h2 className="subText"> FRONT END </h2>
+                </div>
+                <div style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <h3> BOM </h3>
+                    <img src={bom} style={{ objectFit: "cover", width: "100%" }} />
+                    <h2 className="subText"> FRONT END </h2>
+                </div>
+                <div style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <h3> BOAT </h3>
+                    <img src={boat} style={{ objectFit: "cover", width: "100%" }} />
+                    <h2 className="subText"> FRONT END </h2>
+                </div>
+                <div style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <h3> TOEY </h3>
+                    <img src={toey} style={{ objectFit: "cover", width: "100%" }} />
+                    <h2 className="subText"> INFRASTRUCTURE </h2>
+                </div>
+            </div>
+            <div style={{ width: "100vw", height: "200px", overflow: "hidden", display: "flex", alignItems: "flex-end" }} >
+                <img src={fence} />
+            </div>
         </div>
-       </div>
-        
- 
     )
- 
- 
- }
- export default Pageten
+}
+export default pageTen2
