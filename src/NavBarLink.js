bd458615e923e420ba6460fd967904bb722aa59a
Fah <phurichayakhem1@gmail.com>
2021-11-17 21:46:54 +0700

    navbar update

--- src/NavBarLink.js
+++ src/NavBarLink.js
@@ -5,6 +5,7 @@ const NavBarLink = ({ href, childrenavigator, isactive, children }) => {
     return (
         <li style={{ padding: "14px 16px" }} onMouseEnter={() => setishover(true)} onMouseLeave={() => setishover(false)} >
             <a className={ishover ? 'bg-active' : 'bg'} href={href}>{children}</a>
+
         </li>
     )
 }
